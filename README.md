1f0bd2aac0ffbbcfa0eb202b408d86ba1e5b2537
Michael <sunhs_work@163.com>
2020-08-26 10:41:38 +0800

    Initial Commit. 1.This version implements "Hello world". 2.Implement how to read configuration from spring boot configuration file.

---
+++ README.md
@@ -0,0 +1,3 @@
+# What is it?
+
+This is project which demonstrate how to use spring boot.
---
+++ src/test/java/cn/michael/HelloTest.java
@@ -0,0 +1,21 @@
+package cn.michael;
+
+import cn.michael.bean.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloTest {
+
+    @Autowired
+    Person person;
+
+    @Test
+    public void contextLoad(){
+        System.out.println(person);
+    }
+}
